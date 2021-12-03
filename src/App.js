1787c2676e0ba1231c7254accf00c038c132978a
Pazmy <fadhillah17azmy01@gmail.com>
2021-12-03 21:18:55 +0700

    init app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import styled from "styled-components";
+import Users from "./components/Pages/Users";
+import Movies from "./components/Pages/Movies";
+import Orders from "./components/Pages/Orders";
+import AddMovie from "./components/Pages/AddMovie";
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr 1fr 1fr;
+  grid-template-rows: 60px 200px 320px;
+  width: 100%;
+`;
+const Content = styled.div`
+  grid-row: 2/4;
+  grid-column: 2/5;
+  /* overflow-y: auto;
+  overflow-x: auto; */
+  overflow: auto;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Sidebar />
+        <Header />
+        <Content>
+          <Routes>
+            <Route path="/users" element={<Users />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/movies/add" element={<AddMovie />} />
+          </Routes>
+        </Content>
+      </Container>
+    </Router>
   );
 }
 
